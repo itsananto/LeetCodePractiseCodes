b3b2d94700704d46ecf61ce121151c729ad765c3
Ananto Ghosh <its.ananto@gmail.com>
2018-05-08 01:18:27 +0600

    First commit

---
+++ LeetCode/UniqueMorseRepresentationsSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class UniqueMorseRepresentationsSolution
+    {
+        string[] input = new string[] { "gin", "zen", "gig", "msg" };
+
+        public UniqueMorseRepresentationsSolution()
+        {
+            int output = UniqueMorseRepresentations(input);
+            Console.WriteLine(output);
+        }
+
+        //accepted
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] morseCodeArray = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            Dictionary<string, int> hash = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (char c in word)
+                {
+                    sb.Append(morseCodeArray[c - 'a']);
+                }
+                string key = sb.ToString();
+
+                hash[key] = 1;
+
+            }
+
+            return hash.Count;
+        }
+    }
+}
