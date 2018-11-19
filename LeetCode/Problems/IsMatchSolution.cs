d26c10edb13b5743a6e15c58a1243468a800c16b
Ananto Ghosh <Ananto Ghosh its.ananto@gmail.com>
2018-11-20 00:35:44 +0600

    no message

---
+++ LeetCode/Problems/IsMatchSolution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    class IsMatchSolution
+    {
+        public IsMatchSolution()
+        {
+            IsMatch("aab", "c*a*b");
+        }
+
+        public bool IsMatch(string s, string p)
+        {
+            int i = 0;
+            bool match = true;
+            for (int j = 0; j < p.Length; j++)
+            {
+                if (i >= s.Length && p[j] != '*') return false;
+
+                if (p[j] == '.')
+                {
+                    ++i;
+                }
+                else
+                {
+                    if (p[j] == '*')
+                    {
+                        if (j == 0) return false;
+                        char prev = p[j - 1];
+
+                        while (i < s.Length && s[i] == prev)
+                        {
+                            ++i;
+                        }
+
+                    }
+                    else
+                    {
+                        if (p[j] != s[i]) return false;
+                        ++i;
+                    }
+                }
+
+                if (i < s.Length) return false;
+
+                return match;
+            }
+        }
+    }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -11,7 +11,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            new DecodeAtIndexSolution();
+            new IsMatchSolution();
         }
     }
 }
